d8abb5e7ba6f82f527c82e35a2f2d1820feda7b8
Anant Patel <anantpatel07012003@gmail.com>
2022-12-30 12:29:08 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/styles.css";
-import Keeper from "./Keeper.jsx"
-ReactDOM.render(
-    <Keeper />
-,document.getElementById("root"));
+import Keeper from "./Keeper.jsx";
+ReactDOM.render(<Keeper />, document.getElementById("root"));
