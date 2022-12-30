21a762a4fa1434fcd91f770c0ec7043a0c3b59bb
Anant <anantpatel07012003@gmail.com>
2022-12-30 16:47:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/index.css";
+import Keeper from "./Keeper.jsx"
+ReactDOM.render(
+    <Keeper />
+,document.getElementById("root"));
